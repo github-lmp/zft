f10bad89dec4e44109bfe2a2ea6525e8efd68297
zjf <zjf@zjf-laptop.(none)>
2011-11-02 09:25:41 +0800

    看了webob

--- swift_test/swift_demo.py
+++ swift_test/swift_demo.py
@@ -25,7 +25,8 @@ import config
 
 urls=(
     '/','Index',
-    '/login.html','Login',
+    '/login','Login',
+    '/logout', 'Logout',
     '/ccont','Ccont',
     '/dcont','Dcont',
     '/upload_object','Upload_object',
@@ -35,6 +36,7 @@ urls=(
 
 app = web.application(urls,globals())
 render = web.template.render('templates')
+
 if web.config.get('_session') is None:
     session = web.session.Session(\
             app,web.session.DiskStore('sessions'),\
@@ -47,84 +49,76 @@ else:
 #让子应用可以使用session。
 def session_hook():
     web.ctx.session = session
-app.add_processor(web.loadhook(session_hook))
 
 
 def login(func):
     """登录返回执行func,，否则返回login页面"""
     def newFunc(*args, **kwargs):
+        session = web.ctx.session
         if session.logged==1:
             return func(*args, **kwargs)
         else:
             return render.login()
     return newFunc
 
+class Logout:
+    def GET(self):
+        web.ctx.session.kill()
+        raise web.seeother('/')
+
 class Login:
     def GET(self):
         x = web.input()
-        if "do" in x.keys() and x["do"] == "logout":
-            session.kill()
-            web.seeother("/")
     def POST(self):
         x = web.input()
-        print "login error.", x
         account = x["account"]
         username = x["username"]
         password = x["password"]
-        if account == config.account and username == \
-                config.username and password == config.password:
-            session.logged = 1
-            h = httplib2.Http(".cache")
-            respl = []
-            contentl = []
-            XStorageUser = account + ":" + username
-            XStoragePass = password
-            print "http://" + config.swiftHost + "/auth/v1.0"
-            resp, content = h.request("http://" + config.swiftHost + \
-                    "/auth/v1.0", "GET", headers={"X-Storage-User":\
-                    XStorageUser, "X-Storage-Pass":XStoragePass})
-            print resp["x-auth-token"]
-            config.XAuthToken = resp["x-auth-token"]
-            session.XAuthToken = resp["x-auth-token"]
-            print resp, content
-            web.seeother("/")
+
+        h = httplib2.Http(".cache")
+        respl = []
+        contentl = []
+        XStorageUser = account + ":" + username
+        XStoragePass = password
+        resp, content = h.request("http://" + config.swiftHost + \
+                "/auth/v1.0", "GET", headers={"X-Storage-User":\
+                 XStorageUser, "X-Storage-Pass":XStoragePass})
+        print resp, resp.status, content
+        if resp.status == 200:
+            web.ctx.session.logged = 1
         else:
-            print  x["account"], x["username"], x["password"]
             return "login error."
+        web.ctx.session.XAuthToken = resp["x-auth-token"]
+        web.ctx.session.XStorageUrl = resp["x-storage-url"].replace(
+                    '127.0.0.1:8080',  config.swiftHost)
+        raise web.seeother("/")
+
 
 class Index:
     @login
     def GET(self):
         x = web.input()
-        print x
-        print dict(web.ctx.env)
-        env = dict(web.ctx.env)
-#        req = Request(env)
-#        print "req", req
-#        print "req.headers", req.headers
-#        reqSwift = Request.blank(config.XStorageUrl)
-#        req.headers["X-Auth-Token"] = config.XAuthToken
-#        print "req, req.envirom,", req, req.headers
         h = httplib2.Http(".cache")
         respl = []
         contentl = []
-        resp, content = h.request(config.XStorageUrl, "GET", \
-                headers={"X-Auth-Token":config.XAuthToken})
+        print '----------', web.ctx.session.XStorageUrl, web.ctx.session.XAuthToken
+        resp, content =  h.request(
+                web.ctx.session.XStorageUrl, "GET", \
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
         container = content.split("\n")[:-1]
-        print resp, content
+        print 'resp:', resp, content
         respl.append(resp)
         contentl.append(container)
         for i in range(int(resp['x-account-container-count'])):
-            print i, container[i] 
-            resp, content = h.request(config.XStorageUrl + "/" + \
+            print i, container[i]
+            resp, content = h.request(web.ctx.session.XStorageUrl + "/" + \
                     container[i], "GET", \
-                headers={"X-Auth-Token":config.XAuthToken})
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
             print resp, content
             respl.append(resp)
             contentl.append(container[i] + ":" + \
                     str(content.split("\n")[:-1]))
         return render.index(respl, contentl)
-      
 
 
 class Ccont:
@@ -132,11 +126,11 @@ class Ccont:
     def POST(self):
         x = web.input()
         print x
-        ccont_name = x["ccont_name"] 
+        ccont_name = x["ccont_name"]
         h = httplib2.Http(".cache")
-        resp, content = h.request(config.XStorageUrl + "/" + \
+        resp, content = h.request(web.ctx.session.XStorageUrl + "/" + \
                 ccont_name, "PUT", \
-                headers={"X-Auth-Token":config.XAuthToken})
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
         print resp, content
         return render.index(resp, content)
 
@@ -146,11 +140,11 @@ class Dcont:
     def POST(self):
         x = web.input()
         print x
-        ccont_name = x["ccont_name"] 
+        ccont_name = x["ccont_name"]
         h = httplib2.Http(".cache")
-        resp, content = h.request(config.XStorageUrl + "/" + \
+        resp, content = h.request(web.ctx.session.XStorageUrl + "/" + \
                 ccont_name, "DELETE", \
-                headers={"X-Auth-Token":config.XAuthToken})
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
         print resp, content
         return render.index(resp, content)
 
@@ -160,12 +154,12 @@ class Upload_object:
     def POST(self):
         x = web.input()
         data = x["file"]
-        ccont_name = x["ccont_name"] 
-        filename = x["filename"] 
+        ccont_name = x["ccont_name"]
+        filename = x["filename"]
         h = httplib2.Http(".cache")
-        resp, content = h.request(config.XStorageUrl + "/" + \
+        resp, content = h.request(web.ctx.session.XStorageUrl + "/" + \
                 ccont_name + "/" + filename, "PUT", data,\
-                headers={"X-Auth-Token":config.XAuthToken})
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
         print resp, content
         return render.index(resp, content)
 
@@ -173,13 +167,13 @@ class Delete_object:
     """    """
     def POST(self):
         x = web.input()
-        ccont_name = x["ccont_name"] 
-        filename = x["filename"] 
+        ccont_name = x["ccont_name"]
+        filename = x["filename"]
         h = httplib2.Http(".cache")
-        resp, content = h.request(config.XStorageUrl + \
+        resp, content = h.request(web.ctx.session.XStorageUrl + \
                 "/" + ccont_name + \
                 "/" + filename, "DELETE", \
-                headers={"X-Auth-Token":config.XAuthToken})
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
         print resp, content
         return render.index(resp, content)
 
@@ -187,20 +181,16 @@ class Retrieve_object:
     """    """
     def POST(self):
         x = web.input()
-        ccont_name = x["ccont_name"] 
-        filename = x["filename"] 
+        ccont_name = x["ccont_name"]
+        filename = x["filename"]
         h = httplib2.Http(".cache")
-#        web.header("Content-Type", "application/json")
-#        return json.dumps({"url": config.XStorageUrl + "/" + \
-#                ccont_name + "/" + filename, "method": "GET", \
-#                "X-Auth-Token": config.XAuthToken })
-#        print resp, content
-        resp, content = h.request(config.XStorageUrl + "/" + \
+        resp, content = h.request(web.ctx.session.XStorageUrl + "/" + \
                 ccont_name + "/" + filename, "GET", \
-                headers={"X-Auth-Token":config.XAuthToken})
+                headers={"X-Auth-Token":web.ctx.session.XAuthToken})
         return content
 #        return render.index(resp, content)
 
 
 if __name__=="__main__":
+    app.add_processor(web.loadhook(session_hook))
     app.run()
